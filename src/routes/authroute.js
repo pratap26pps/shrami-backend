aef7560cfd02ffb37280d3d7341cedf506c92de5
pratap26pps <pankajpatna10321@gmail.com>
2026-01-03 17:37:29 +0530

    remove the google auth creation

--- src/routes/authroute.js
+++ src/routes/authroute.js
@@ -3,14 +3,10 @@ import express from "express";
 const router = express.Router();
 import { googleLogin,
    callback,
-   googleauthcreation,
    verifyOtpAndSignup,LoginHandler,forgotPassword,resetPassword } from "../controllers/authcontrollers.js";
  
 router.get("/google", googleLogin);
 router.get("/callback/google", callback);
-
-router.post("/googleauthcreation", googleauthcreation);
-
 
 // manually registration process
 
