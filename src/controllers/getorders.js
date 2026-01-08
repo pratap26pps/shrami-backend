905670b94a05e779f2053a0f9f07701192e69d11
pratap26pps <pankajpatna10321@gmail.com>
2026-01-08 22:36:49 +0530

    getorder added

--- src/controllers/getorders.js
+++ src/controllers/getorders.js
@@ -8,8 +8,7 @@ export const getOrders = async (req, res) => {
   try {
     await connectDB();
     const orders = await Order.find()
-      .populate('user', 'firstName lastName email')
-      .populate('items.product', 'name price images');
+    
     res.status(200).json({ orders });
   } catch (error) {
     res.status(500).json({ message: 'Failed to fetch orders', error: error.message });
